9f4b14deba24b95c7f89b32c7c4fdb52ead083e5
rajnish <rajnishprasadkalwar@gmail.com>
2022-12-15 18:16:03 +0000

    merge arrays using stl

---
+++ move_Zeros.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        for(int i=0;i<nums.size()-1;i++){
+            for(int j=i+1;j<=nums.size()-1;j++){
+                if(nums[i]==0){
+                    swap(nums[i],nums[j]);
+                }
+                else 
+                break;
+            }
+        }
+        
+    }
+};
